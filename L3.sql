60f6d1cdd12d4c48925b631eb66780a0abef432a
Babin <155212596+Babin-exe@users.noreply.github.com>
2026-01-03 19:38:40 +0545

    Update L3.sql

--- L3.sql
+++ L3.sql
@@ -33,14 +33,60 @@ SELECT S_Address
 FROM College
 WHERE S_name LIKE '_a%r_';
 
+
 -- Display name of all students whose name contains exactly four letters.
 SELECT S_name
 FROM College
 WHERE S_name LIKE '____';
 
+-- Display information of those students whose name start with letter ‘H’ and contains exactly four letters.
+SELECT *
+FROM College
+WHERE S_name LIKE 'H___';
+
+
+-- Display information of those students whose name start with letter ‘H’ or ‘s’ and contains exactly four letters.
+SELECT *
+FROM College
+WHERE S_name LIKE 'H___' 
+OR S_name LIKE 'S___';
+
+-- Display information of those students whose name start with letter ‘S’ and ends with ‘a’ and contains
+-- exactly four letters.
+SELECT *
+FROM College
+WHERE S_name LIKE 'S__a';
+
+
+-- Display name address and age of all students who lives in Kathmandu and are at most 20 years old.
+SELECT S_name, S_Address, Age
+FROM College
+WHERE S_Address = 'Kathmandu' 
+  AND Age <= 20;
+
+
+-- Display information about all students in descending order of age.
+SELECT *
+FROM College
+ORDER BY Age DESC;
+
+
+-- Display id, name, age and address of all students in ascending order of their age.
+SELECT S_id, S_name, Age, S_Address
+FROM College
+ORDER BY Age ASC;
+
+
+-- Display record of all students in descending order of name.
+SELECT *
+FROM College
+ORDER BY S_name DESC;
 
 
 
+-- Select distinct (not repeated) address from table [i.e. no address should be repeated in list.
+SELECT DISTINCT S_Address
+FROM College;
 
 
 
